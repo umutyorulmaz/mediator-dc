953fbf2c73a830d43a4f82176f8da64ea4e28628
umut yorulmaz <umutyorulmaz93@gmail.com>
2024-06-13 21:54:08 -0400

    push notifications

--- pre-requisite.sh
+++ pre-requisite.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the Node.js script to manage PostgreSQL container
-node ~/mediator/manage-postgres.js
+node /Users/user/digi-cred-dev/dc-chapter2/exp/mediator-dc/manage-postgres.js
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { CreateAgentDto } from './dto/create-agent.dto';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { CreateAgentDto } from "./dto/create-agent.dto";
 import {
   Agent,
   CacheModule,
@@ -9,158 +9,220 @@ import {
   InMemoryLruCache,
   MediatorModule,
   InitConfig,
-  WsOutboundTransport
-  } from '@credo-ts/core'
-import { 
-    ProofEventTypes, 
-    ProofStateChangedEvent, 
-    BasicMessageRole,
-    ConnectionStateChangedEvent,
-    ConnectionEventTypes,
-    DidExchangeState,
-    BasicMessageStateChangedEvent,
-    BasicMessageEventTypes,
-    ConsoleLogger, 
-    LogLevel
-  } from '@aries-framework/core';
-import { AskarModule, AskarMultiWalletDatabaseScheme  } from '@credo-ts/askar';
-import { ariesAskar } from '@hyperledger/aries-askar-nodejs';
-import { Server } from 'ws';
-import { HttpInboundTransport, agentDependencies, WsInboundTransport } from '@credo-ts/node'
-import type { Socket } from 'net'
-import { askarPostgresConfig } from './database';
+  WsOutboundTransport,
+} from "@credo-ts/core";
+import {
+  ProofEventTypes,
+  ProofStateChangedEvent,
+  BasicMessageRole,
+  ConnectionStateChangedEvent,
+  ConnectionEventTypes,
+  DidExchangeState,
+  BasicMessageStateChangedEvent,
+  BasicMessageEventTypes,
+  ConsoleLogger,
+  LogLevel,
+} from "@aries-framework/core";
+import { AskarModule, AskarMultiWalletDatabaseScheme } from "@credo-ts/askar";
+import { ariesAskar } from "@hyperledger/aries-askar-nodejs";
+import { Server } from "ws";
+import {
+  HttpInboundTransport,
+  agentDependencies,
+  WsInboundTransport,
+} from "@credo-ts/node";
+import type { Socket } from "net";
+import { askarPostgresConfig } from "./database";
+import {
+  PushNotificationsApnsModule,
+  PushNotificationsFcmModule,
+} from "@credo-ts/push-notifications";
 
 @Injectable()
 export class AppService {
-
   agentConfig: InitConfig;
   agent: Agent;
   socketServer: Server;
 
   constructor(private configService: ConfigService) {}
-  
+
   async startAgent(createAgentDto: CreateAgentDto): Promise<string> {
-      console.log("Agent DTO=", createAgentDto)
+    console.log("Agent DTO=", createAgentDto);
 
-      // Create PostgreSQL storage configuration
-      const storageConfig = askarPostgresConfig(this.configService);
+    // Create PostgreSQL storage configuration
+    const storageConfig = askarPostgresConfig(this.configService);
 
+    // Setup the configuration for the agent
+    this.agentConfig = {
+      endpoints: [createAgentDto.endpoint],
+      label: createAgentDto.label,
+      walletConfig: {
+        id: createAgentDto.walletId,
+        key: createAgentDto.walletKey,
+        storage: storageConfig,
+      },
+      logger: new ConsoleLogger(LogLevel.info),
+    };
 
-      // Setup the configuration for the agent
-      this.agentConfig = {
-        endpoints: [createAgentDto.endpoint],
-        label: createAgentDto.label,
-        walletConfig: {
-          id: createAgentDto.walletId,
-          key: createAgentDto.walletKey,
-          storage:storageConfig,
-        },
-        logger: new ConsoleLogger(LogLevel.info)
-      }
-  
-      // Set up agent
-      this.agent = new Agent({
-        config: this.agentConfig,
-        dependencies: agentDependencies,
-        modules: {
-          askar: new AskarModule({ ariesAskar,
-            multiWalletDatabaseScheme: AskarMultiWalletDatabaseScheme.ProfilePerWallet,
-           }),
-          mediator: new MediatorModule({
-            autoAcceptMediationRequests: true,
-          }),
-          connections: new ConnectionsModule({
-            autoAcceptConnections: true,
-          }),
-          cache: new CacheModule({
-            cache: new InMemoryLruCache({ limit: 500 }),
-          }),
-        },
-      });
-  
-      // Initialize websocket server
-      this.socketServer = new Server({ noServer: true });
-      console.log("Created socketServer");
-  
-      // Create all transports
-      const httpInboundTransport = new HttpInboundTransport({port: createAgentDto.port })
-      const httpOutboundTransport = new HttpOutboundTransport()
-      const wsInboundTransport = new WsInboundTransport({ server: this.socketServer })
-      const wsOutboundTransport = new WsOutboundTransport()
-      console.log("Created transports");
-  
-      // Register all Transports
-      this.agent.registerInboundTransport(httpInboundTransport)
-      this.agent.registerOutboundTransport(httpOutboundTransport)
-      this.agent.registerInboundTransport(wsInboundTransport)
-      this.agent.registerOutboundTransport(wsOutboundTransport)
-      console.log("Registered transports");
-  
-      // Initialize agent
-      console.log("Agent pre-initialize");
-      await this.agent.initialize().catch(console.error);
-      console.log("Agent initialized");
-  
-      httpInboundTransport.server?.on('upgrade', (request, socket, head) => {
-        this.socketServer.handleUpgrade(request, socket as Socket, head, (socket) => {
-          this.socketServer.emit('connection', socket, request)
-        })
-      })
-  
-      return "OK";
- }
-  
+    // Set up agent
+    this.agent = new Agent({
+      config: this.agentConfig,
+      dependencies: agentDependencies,
+      modules: {
+        askar: new AskarModule({
+          ariesAskar,
+          multiWalletDatabaseScheme:
+            AskarMultiWalletDatabaseScheme.ProfilePerWallet,
+        }),
+        mediator: new MediatorModule({
+          autoAcceptMediationRequests: true,
+        }),
+        connections: new ConnectionsModule({
+          autoAcceptConnections: true,
+        }),
+        cache: new CacheModule({
+          cache: new InMemoryLruCache({ limit: 500 }),
+        }),
+        pushNotificationsApns: new PushNotificationsApnsModule(),
+        pushNotificationsFcm: new PushNotificationsFcmModule(),
+      },
+    });
+
+    // Initialize websocket server
+    this.socketServer = new Server({ noServer: true });
+    console.log("Created socketServer");
+
+    // Create all transports
+    const httpInboundTransport = new HttpInboundTransport({
+      port: createAgentDto.port,
+    });
+    const httpOutboundTransport = new HttpOutboundTransport();
+    const wsInboundTransport = new WsInboundTransport({
+      server: this.socketServer,
+    });
+    const wsOutboundTransport = new WsOutboundTransport();
+    console.log("Created transports");
+
+    // Register all Transports
+    this.agent.registerInboundTransport(httpInboundTransport);
+    this.agent.registerOutboundTransport(httpOutboundTransport);
+    this.agent.registerInboundTransport(wsInboundTransport);
+    this.agent.registerOutboundTransport(wsOutboundTransport);
+    console.log("Registered transports");
+
+    // Initialize agent
+    console.log("Agent pre-initialize");
+    await this.agent.initialize().catch(console.error);
+    console.log("Agent initialized");
+
+    // To send apns device info to another agent you have to acquire the device token and send it.
+    // await this.agent.modules.pushNotificationsApns.sendDeviceInfo(
+    //   "a-valid-connection-id",
+    //   {
+    //     deviceToken:
+    //       "fArXyVZHSpiGdzGZXdo7UB:APA91bHdGgyxDyGvM4JmdacgZ5C9HYvh-HhISJp9Q2CSm_BMK2u_PwymKXZOlKlkcMmiIYeeVRQWkx4_T4U-oArayxX6HMyd8l3cvwtVo2MROv6DX_rGbefLzhQCZqK1e3Xh4DKPEU16",
+    //   }
+    // );
+
+    // // To get the device info and the used service back from the other agent
+    // await this.agent.modules.pushNotificationsApns.getDeviceInfo(
+    //   "a-valid-connection"
+    // );
+
+    // /* -- fcm / Android -- */
+
+    //To send fcm, primarily Android, device info to another agent you have to acquire the device token and send it.
+    // await this.agent.modules.pushNotificationsFcm.sendDeviceInfo(
+    //   "a-valid-connection-id",
+    //   {
+    //     deviceToken:
+    //       "fArXyVZHSpiGdzGZXdo7UB:APA91bHdGgyxDyGvM4JmdacgZ5C9HYvh-HhISJp9Q2CSm_BMK2u_PwymKXZOlKlkcMmiIYeeVRQWkx4_T4U-oArayxX6HMyd8l3cvwtVo2MROv6DX_rGbefLzhQCZqK1e3Xh4DKPEU16",
+    //   }
+    // );
+
+    //To get the device info and the used service back from the other agent
+
+    httpInboundTransport.server?.on("upgrade", (request, socket, head) => {
+      this.socketServer.handleUpgrade(
+        request,
+        socket as Socket,
+        head,
+        (socket) => {
+          this.socketServer.emit("connection", socket, request);
+        }
+      );
+    });
+
+    return "OK";
+  }
 
   async createInvitation(): Promise<String> {
-    var outOfBandRecord = await this.agent.oob.createLegacyInvitation()
+    var outOfBandRecord = await this.agent.oob.createLegacyInvitation();
     var invite = {
-      invitationUrl: outOfBandRecord.invitation.toUrl({ domain: this.agent.config.endpoints[0] }),
-      outOfBandRecord
-    }
-    return invite.invitationUrl
+      invitationUrl: outOfBandRecord.invitation.toUrl({
+        domain: this.agent.config.endpoints[0],
+      }),
+      outOfBandRecord,
+    };
+    return invite.invitationUrl;
   }
 
   async createOOBInvitation(): Promise<String> {
-    const outOfBandRecord = await this.agent.oob.createInvitation({ multiUseInvitation: true })
+    const outOfBandRecord = await this.agent.oob.createInvitation({
+      multiUseInvitation: true,
+    });
     var invite = {
-      invitationUrl: outOfBandRecord.outOfBandInvitation.toUrl({ domain: this.agent.config.endpoints[0] }),
+      invitationUrl: outOfBandRecord.outOfBandInvitation.toUrl({
+        domain: this.agent.config.endpoints[0],
+      }),
       outOfBandRecord,
-    }
-    return invite.invitationUrl
+    };
+    return invite.invitationUrl;
   }
 
-
   setupProofRequestListener() {
-    console.log("Listen for proof")
-    this.agent.events.on(ProofEventTypes.ProofStateChanged, async ({ payload }: ProofStateChangedEvent) => {
-      //console.log("Proof presentation=",payload.proofRecord )
-      console.log("Proof state: ", payload.proofRecord?.state)
-      console.log("Proof verified: ", payload.proofRecord?.isVerified ? 'Verified' : 'not Verified')
-    })
+    console.log("Listen for proof");
+    this.agent.events.on(
+      ProofEventTypes.ProofStateChanged,
+      async ({ payload }: ProofStateChangedEvent) => {
+        //console.log("Proof presentation=",payload.proofRecord )
+        console.log("Proof state: ", payload.proofRecord?.state);
+        console.log(
+          "Proof verified: ",
+          payload.proofRecord?.isVerified ? "Verified" : "not Verified"
+        );
+      }
+    );
   }
 
   setupMessageListener() {
-    console.log("Listen for messages")
-    this.agent.events.on(BasicMessageEventTypes.BasicMessageStateChanged, async ({ payload }: BasicMessageStateChangedEvent) => {
-      if (payload.basicMessageRecord.role === BasicMessageRole.Receiver) {
-        console.log("Message:",payload.message.content);
+    console.log("Listen for messages");
+    this.agent.events.on(
+      BasicMessageEventTypes.BasicMessageStateChanged,
+      async ({ payload }: BasicMessageStateChangedEvent) => {
+        if (payload.basicMessageRecord.role === BasicMessageRole.Receiver) {
+          console.log("Message:", payload.message.content);
+        }
       }
-    })
+    );
   }
 
   setupConnectionListener = () => {
-    this.agent.events.on<ConnectionStateChangedEvent>(ConnectionEventTypes.ConnectionStateChanged, ({ payload }) => {
-      if (payload.connectionRecord.state === DidExchangeState.Completed) {
-        // the connection is now ready for usage in other protocols!
-        console.log("Connection completed", payload.connectionRecord)
-        //this.afjAgent.connection_id = payload.connectionRecord.id
-        //process.exit(0)
+    this.agent.events.on<ConnectionStateChangedEvent>(
+      ConnectionEventTypes.ConnectionStateChanged,
+      async ({ payload }) => {
+        if (payload.connectionRecord.state === DidExchangeState.Completed) {
+          // the connection is now ready for usage in other protocols!
+          console.log("Connection completed", payload.connectionRecord);
+          //this.afjAgent.connection_id = payload.connectionRecord.id
+          //process.exit(0)
+        } else {
+          console.log("Connection status", payload.connectionRecord);
+        }
+        await this.agent.modules.pushNotificationsFcm.getDeviceInfo(
+          "a-valid-connection-id"
+        );
       }
-      else {
-        console.log("Connection status", payload.connectionRecord)
-      }
-    })
-  }
-
+    );
+  };
 }
-
